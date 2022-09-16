d2e4cf99e45063e467d3975978df53904c94e28b
Dragan Jovanović <draganele@gmail.com>
2022-09-16 19:03:50 +0300

    added sudo for creating daemon file

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo chown $(whoami) ~/.copycat
 sudo cp -r ./target/release/copycat /usr/local/bin/
 sudo cp -r ./popup.py /usr/local/bin/
 
-echo "<?xml version="1.0" encoding="UTF-8"?>
+sudo echo "<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
