a2d85adc6deb6b5b1b3885e61b23d1557f085dff
Dragan Jovanović <draganele@gmail.com>
2022-09-16 18:43:05 +0300

    removed sudo from install.sh :)

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ pip install -r requirements.txt
 
 launchctl remove com.copycat.meraxes
 
-sudo touch ~/.copycat
+touch ~/.copycat
 
-sudo cp -r ./target/release/copycat /usr/local/bin/
-sudo cp -r ./popup.py /usr/local/bin/
+cp -r ./target/release/copycat /usr/local/bin/
+cp -r ./popup.py /usr/local/bin/
 
 echo "<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
