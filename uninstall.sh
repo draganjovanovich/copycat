bcec00d2f0a364f03f8de3e56fd00ea7b9e4023e
Dragan Jovanović <draganele@gmail.com>
2022-09-19 15:44:33 +0300

    added sudo for rm files in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 
-rm ~/.copycat
-rm  /usr/local/bin/copycat
-rm  /usr/local/bin/popup.py
-rm ~/Library/LaunchAgents/com.copycat.meraxes.plist
+sudo rm ~/.copycat
+sudo rm  /usr/local/bin/copycat
+sudo rm  /usr/local/bin/popup.py
+sudo rm ~/Library/LaunchAgents/com.copycat.meraxes.plist
 
 launchctl remove com.copycat.meraxes
